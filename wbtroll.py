42a8552d0e101a570ecffc38c33a9cc040ad117e
TheMrRedSlime <69750444+TheMrRedSlime@users.noreply.github.com>
2025-10-22 11:31:44 +0530

    Beautify

--- wbtroll.py
+++ wbtroll.py
@@ -1,9 +1,11 @@
-import aiohttp
 import asyncio
+
+import aiohttp
+
 
 async def snap_discord_webhook(webhooker_url):
     async with aiohttp.ClientSession() as session:
-        async with session.delete(url) as response:
+        async with session.delete(url=webhooker_url) as response:
             if response.status == 204:
                 print("Webhook successfully deleted!")
             elif response.status == 404:
@@ -12,10 +14,10 @@ async def snap_discord_webhook(webhooker_url):
                 print("You are currently being ratelimited! Try again later")
             else:
                 print(f"Failed to send message. Status code: {response.status}")
+
+
 async def send_discord_webhook(webhook_url, message):
-    data = {
-        "content": message
-    }
+    data = {"content": message}
     async with aiohttp.ClientSession() as session:
         while True:
             async with session.post(url=webhook_url, data=data) as response:
@@ -27,6 +29,7 @@ async def send_discord_webhook(webhook_url, message):
                     print("You are currently being ratelimited! Try again later")
                 else:
                     print(f"Failed to send message. Status code: {response.status}")
+
 
 async def main():
     while True:
@@ -57,6 +60,7 @@ async def main():
 
         else:
             print("Invalid choice. Please select a valid option.")
+
 
 if __name__ == "__main__":
     try:
